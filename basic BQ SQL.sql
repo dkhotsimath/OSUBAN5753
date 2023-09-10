6b56e7dc4cdbeab75d80eedb432e7fb4e26aa95f
dkhotsimath <72765537+dkhotsimath@users.noreply.github.com>
2023-09-09 20:54:01 -0500

    Update basic BQ SQL.sql

--- basic BQ SQL.sql
+++ basic BQ SQL.sql
@@ -13,3 +13,15 @@ AS
 SELECT *,     
  TIMESTAMP_SECONDS(time) AS snapshotdatetime 
 FROM SampleDataset.CryptoHistDataFull
+
+
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_info(
+# enter service account credentials here
+,
+)
+
+sql="SELECT * FROM `osudemoproject-341819.SampleDataset.CryptoHistwithTime` LIMIT 10"
+df = pandas_gbq.read_gbq(sql, project_id="osudemoproject-341819", credentials=credentials)
+df.head()
